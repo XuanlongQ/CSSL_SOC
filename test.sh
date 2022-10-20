825a4587a46dffe6b00d74a2e4ec855d01ce4e0b
Xuanlong <qxlpku@gmail.com>
2022-10-20 20:06:36 +0800

    add slurm script

--- test.sh
+++ test.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=xuanlong@link.cuhk.edu.hk
+#SBATCH --mail-user=xuanlong@link.cuhk.edu.hk,yjli@link.cuhk.edu.hk
 #SBATCH --job-name=xl-weiboscraping
 #SBATCH --output=scraping.out
 #SBATCH --time=72:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=1500M
-
-# module load python/3.9.11
-
-# srun python 
-
-
 
 nohup scrapy crawl search 
